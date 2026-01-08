1d50c622f9db45598fcf563682b6d5208aaeae7f
Edmond Burnett <_@edmondburnett.com>
2026-01-08 14:50:20 -0800

    add beverage string function

--- src/beverage.rs
+++ src/beverage.rs
@@ -9,11 +9,20 @@ pub enum Beverage {
 #[allow(dead_code)]
 impl Beverage {
     pub fn code(&self) -> u8 {
+        // usage: let code: u8 = Beverage::Coffee.code();
         *self as u8
     }
 
+    pub fn string(&self) -> &str {
+        // let message = format!("I'm drinking {}", beverage.string());
+        match self {
+            Beverage::Water => "water",
+            Beverage::Coffee => "coffee",
+            Beverage::Tea => "tea",
+        }
+    }
+
     pub fn from_code(code: u8) -> Option<Self> {
-        // usage: let code: u8 = Beverage::Coffee.code();
         match code {
             0 => Some(Beverage::Water),
             1 => Some(Beverage::Coffee),
@@ -21,6 +30,4 @@ impl Beverage {
             _ => None,
         }
     }
-
-    // TODO: function to get the effective hydration % of each non-water type
 }
